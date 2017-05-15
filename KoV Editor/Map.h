51a0591c45029b73294745247677b82cf3ca23b1
Aleen1 <alin.calina@yahoo.com>
2017-05-15 18:54:24 +0300

    a

--- KoV Editor/Map.h
+++ KoV Editor/Map.h
@@ -3,15 +3,90 @@
 
 #include "Tile.h"
 #include <algorithm>
+#include <fstream>
 
 class Map {
 	Tile *MapTiles;
+	int *walkable;
+	int width, height;
+
+	ofstream out("map.out");
 
 	Map(int width, int height) {
+		this->width = width;
+		this->height = height;
 		MapTiles = new Tile[width * height];
+		walkable = new int[width * height];
+		
+		for (int i = 0; i < height; ++i) {
+			for (int j = 0; j < width; ++j) {
+				walkable[i * width + j] = 0;
+			}
+		}
 	}
 
 
+	void addWater(int x, int y) {
+		int coord = x * width + y;
+		MapTiles[coord].setCoord(x, y);
+		MapTiles[coord].setHeight(32);
+		MapTiles[coord].setWidth(32);
+
+		Sprite *sprite = new Sprite();
+		sprite->Load("water", 3, 3);
+		MapTiles[coord].setSprite(sprite);
+	}
+
+
+	void addGrass(int x, int y) {
+		int coord = x * width + y;
+		MapTiles[coord].setCoord(x, y);
+		MapTiles[coord].setHeight(32);
+		MapTiles[coord].setWidth(32);
+
+		Sprite *sprite = new Sprite();
+		sprite->Load("grass", 1, 1);
+		MapTiles[coord].setSprite(sprite);
+
+		walkable[coord] = 1;
+	}
+
+
+	void addMine(const char* filename, int x, int y) {
+		int coord = x * width + y;
+		MapTiles[coord].setCoord(x, y);
+		MapTiles[coord].setHeight(64);
+		MapTiles[coord].setWidth(96);
+
+		Sprite *sprite = new Sprite();
+		sprite->Load(filename, 1, 1);
+		MapTiles[coord].setSprite(sprite);
+
+		walkable[(x + 3) * width + y + 1] = 2;
+	}
+
+
+	void addCastle(const char* filename, int x, int y) {
+		int coord = x * width + y;
+		MapTiles[coord].setCoord(x, y);
+		MapTiles[coord].setHeight(192);
+		MapTiles[coord].setWidth(192);
+
+		Sprite *sprite = new Sprite();
+		sprite->Load(filename, 1, 1);
+		MapTiles[coord].setSprite(sprite);
+
+		walkable[(x + 6) * width + y + 3] = 2;
+	}
+
+	void output() {
+		for (int i = 0; i < height; ++i) {
+			for (int j = 0; j < width; ++j) {
+				out << walkable[i][j] << ' ';
+			}
+			out << '\n';
+		}
+	}
 
 };
 
